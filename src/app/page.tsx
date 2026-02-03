81c7cd10ad8dd3e981ad354f3c8306020d948f7f
rplushn <rplushn@gmail.com>
2026-02-02 20:49:11 -0600

    Add ComingSoonModal to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import FeaturesBento from "../components/FeaturesBento";
 import MenuSplit from "../components/MenuSplit";
 import VRewardsSection from "../components/VRewardsSection";
 import Footer from "../components/Footer";
+import ComingSoonModal from "../components/ComingSoonModal";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
+      <ComingSoonModal />
       <Navbar />
       <Hero />
       <FeaturesBento />
